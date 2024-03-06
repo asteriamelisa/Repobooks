f44e618f8a9027fc5be83636b2fa111e157610d3
SalasaNopa <miawboo17@gmail.com>
2024-03-06 17:01:32 +0700

    Update detailbuku dan beranda
    Update detailbuku dan beranda

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('detailbuku', function () {
+    return view('detailbuku');
+});
+
+Route::get('beranda', function () {
+    return view('beranda');
+});
